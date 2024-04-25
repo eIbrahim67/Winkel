b35382f6645654509902511575f4cb21f38d37fe
eIbrahim67 <ibrahim.mohamed.ibrahim.t@gmail.com>
2024-04-25 15:47:12 +0200

    Initial commit

--- app/src/main/java/com/eibrahim/winkel/secondPages/HelpFeedbackActivity.java
+++ app/src/main/java/com/eibrahim/winkel/secondPages/HelpFeedbackActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.eibrahim.winkel.R;
 
 public class HelpFeedbackActivity extends AppCompatActivity {
@@ -12,5 +13,6 @@ public class HelpFeedbackActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_feedback);
+
     }
 }
